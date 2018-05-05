394baa5b62645725218751beb99ac1d745542729
Jay Patel <jabber.wayne@gmail.com>
2018-05-05 16:22:48 -0700

    Add the usercontroller

---
+++ FitMyWay/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FitMyWay.Controllers
+{
+	[RoutePrefix("api/user")]
+	public class UserController : ApiController
+    {
+		[HttpGet]
+		[Route("{userId}/activity/")]
+		public IEnumerable<Library.Activity> GetUserActivitySuggestions(int userId)
+		{
+			var dbConnector = new Library.DBConnector();
+
+			return dbConnector.GetUserActivitySuggestions(userId).Result;
+		}
+	}
+}
