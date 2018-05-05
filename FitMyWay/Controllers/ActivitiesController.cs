61229d230bc61910d6c905222c25d320c59c4d85
Jay Patel <jabber.wayne@gmail.com>
2018-05-05 15:55:59 -0700

    add first api

--- FitMyWay/Controllers/ActivitiesController.cs
+++ FitMyWay/Controllers/ActivitiesController.cs
@@ -11,7 +11,7 @@ namespace FitMyWay.Controllers
 	public class ActivitiesController : ApiController
     {
 		//GetUserActivitySuggestions
-		[HttpPost]
+		[HttpGet]
 		[Route("user/{userId}/all")]
 		public IEnumerable<Library.Activity> GetUserActivitySuggestions(int userId)
 		{
